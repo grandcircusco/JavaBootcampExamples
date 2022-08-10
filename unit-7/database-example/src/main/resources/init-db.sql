1e56c38452cc7c5eeaec8b969e041aff639c7d2c
David Wolverton <dwolvert@gmail.com>
2022-08-10 14:23:46 -0400

    hibernate adjusts db names to lower_snake

--- unit-7/database-example/src/main/resources/init-db.sql
+++ unit-7/database-example/src/main/resources/init-db.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `rooms` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NULL,
-  `maxCapacity` INT NULL,
+  `max_capacity` INT NULL,
   `available` BOOLEAN NULL,
   PRIMARY KEY (`id`));
   
-INSERT INTO `rooms` VALUES 
-(1, 'Annex', 16, true),
-(2, 'Balcony', 24, false),
-(3, 'Penthouse', 50, true);
+INSERT INTO rooms (name, max_capacity, available) VALUES 
+('Annex', 16, true),
+('Balcony', 24, false),
+('Penthouse', 50, true);
