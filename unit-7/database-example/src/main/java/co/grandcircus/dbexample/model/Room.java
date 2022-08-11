e9cf9b3bb0fe033281bab1527b53081acdd32aa8
leemaria <leemariamh@gmail.com>
2022-08-10 23:14:25 -0400

    Fix save on controller

--- unit-7/database-example/src/main/java/co/grandcircus/dbexample/model/Room.java
+++ unit-7/database-example/src/main/java/co/grandcircus/dbexample/model/Room.java
@@ -1,6 +1,8 @@
 package co.grandcircus.dbexample.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -9,6 +11,7 @@ import javax.persistence.Table;
 public class Room {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Integer maxCapacity;
--- unit-7/database-example/src/main/resources/init-db.sql
+++ unit-7/database-example/src/main/resources/init-db.sql
@@ -1,11 +1,10 @@
 CREATE TABLE `rooms` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(255) NULL,
   `max_capacity` INT NULL,
-  `available` BOOLEAN NULL,
-  PRIMARY KEY (`id`));
+  `available` BIT(1) NULL);
   
-INSERT INTO rooms (name, max_capacity, available) VALUES 
+INSERT INTO `rooms` (name, max_capacity, available) VALUES 
 ('Annex', 16, true),
 ('Balcony', 24, false),
 ('Penthouse', 50, true);
