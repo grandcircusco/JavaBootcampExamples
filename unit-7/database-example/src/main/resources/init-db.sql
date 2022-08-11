1844d3096ad084056212721d976ef4871512a3f4
David Wolverton <dwolvert@gmail.com>
2022-08-11 12:05:38 -0400

    updated Rooms init-db.sql

--- unit-7/database-example/src/main/resources/init-db.sql
+++ unit-7/database-example/src/main/resources/init-db.sql
@@ -1,10 +1,16 @@
+-- Copy-paste and run this code in MySQL Workbench to set up the database
+-- table and sample data for this application.
+
 CREATE TABLE `rooms` (
-  `id` BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `name` VARCHAR(255) NULL,
-  `max_capacity` INT NULL,
-  `available` BIT(1) NULL);
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `available` bit(1) DEFAULT NULL,
+  `max_capacity` int DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
   
 INSERT INTO `rooms` (name, max_capacity, available) VALUES 
-('Annex', 16, true),
-('Balcony', 24, false),
-('Penthouse', 50, true);
+('Living Room', 8, true),
+('Home Office', 2, false),
+('Family Room', 6, true),
+('Back Yard', 20, false);
