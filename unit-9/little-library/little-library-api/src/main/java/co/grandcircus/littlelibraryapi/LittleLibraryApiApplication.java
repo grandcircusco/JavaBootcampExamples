82859e099a3491d2eeabaf16b7757b3fcaa3f1de
David Wolverton <dwolvert@gmail.com>
2022-08-16 09:41:34 -0400

    Full Stack example - API

---
+++ unit-9/little-library/little-library-api/src/main/java/co/grandcircus/littlelibraryapi/LittleLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package co.grandcircus.littlelibraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleLibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleLibraryApiApplication.class, args);
+	}
+
+}
