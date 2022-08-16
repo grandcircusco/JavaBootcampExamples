5113d9c4fc51b50212fefee3c2c1e9ab107ffcbe
David Wolverton <dwolvert@gmail.com>
2022-08-16 15:01:53 -0400

    add data.sql to example

---
+++ unit-9/little-library/little-library-api/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO little_library_book (title, author, pages, lent_out) VALUES
+('Pride and Prejudicez', 'Jane Austen', 432, false),
+('To Kill a Mockingbird', 'Harper Lee', 281, true),
+('One Hundred Years of Solitude', 'Gabriel García Márquez', 448, false),
+('In Cold Blood', 'Truman Capote', 368, false),
+('The Adventures of Tom Sawyer', 'Mark Twain', 308, true),
+('The Adventures of Huckleberry Finn', 'Mark Twain', 366, false);
