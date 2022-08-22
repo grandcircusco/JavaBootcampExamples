9f2f45e54c7f99b8ea1f5c943d28fdc15033ebb8
David Wolverton <dwolvert@gmail.com>
2022-08-22 14:35:54 -0400

    Root response for bookshelf-api

--- unit-9/full-stack-bookshelf/bookshelf-api/src/main/java/co/grandcircus/bookshelfapi/controller/BookApiController.java
+++ unit-9/full-stack-bookshelf/bookshelf-api/src/main/java/co/grandcircus/bookshelfapi/controller/BookApiController.java
@@ -1,12 +1,15 @@
 package co.grandcircus.bookshelfapi.controller;
 
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +29,14 @@ public class BookApiController {
 
 	@Autowired
 	private BookRepository repo;
+
+	@RequestMapping("/")
+	public Map<String, Object> home() {
+		Map<String, Object> result = new HashMap<>();
+		result.put("status", "OK");
+		result.put("collections", new String[] { "/books" });
+		return result;
+	}
 	
 	// C(R)UD -- Read All
 	@GetMapping("/books")
