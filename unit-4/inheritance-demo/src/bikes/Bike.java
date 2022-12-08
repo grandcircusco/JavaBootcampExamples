669562020c5431bc26cd1c2de7e4e4e224e36d3f
David Wolverton <dwolvert@gmail.com>
2022-12-08 15:30:22 -0500

    Inheritance Demo

--- unit-4/inheritance-demo/src/bikes/Bike.java
+++ unit-4/inheritance-demo/src/bikes/Bike.java
@@ -2,13 +2,17 @@ package bikes;
 
 public class Bike {
   protected int speed = 0; // speed property starts at 0
+
+  public int getSpeed() {
+    return speed;
+  }
    
   public void go() {
     this.speed++;
   }
 
-  public int getSpeed() {
-    return speed;
+  public void stop() {
+    this.speed = 0;
   }
 
   @Override
